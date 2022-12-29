19427c147a0e7b9fe0c18654c7ee9f23bd7b456b
dibkb <dibas9110@gmail.con>
2022-12-29 16:33:22 +0530

    navbar wiring complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  MouseEvent,
+  MouseEventHandler,
+} from "react";
 import logo from "../public/assets/logo.png";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "../styles/Navbar.module.scss";
+const links = ["Home", "Contact", "Menu", "About Us"];
+const LinksContent = () => {
+  const router = useRouter();
+  const handleClick = (element: string) => {
+    const URL = element.split(" ")[0].toLocaleLowerCase();
+    router.push(URL === "home" ? "/" : URL);
+  };
+  return (
+    <div className={styles["linkContent"]}>
+      {links.map((element) => {
+        return (
+          <a
+            key={element}
+            className={styles.link}
+            onClick={(e) => {
+              e.preventDefault();
+              handleClick(element);
+            }}
+          >
+            {element}
+          </a>
+        );
+      })}
+      ;
+    </div>
+  );
+};
+
 const Navbar = () => {
   return (
-    <nav className={styles.navbar}>
-      <Image src={logo.src} alt="Logo" width={150} height={125}></Image>
-    </nav>
+    <main className={styles.navbar__Container}>
+      <nav className={styles.nav}>
+        <Image src={logo.src} alt="Logo" width={150} height={125}></Image>
+      </nav>
+      <LinksContent />
+    </main>
   );
 };
 
